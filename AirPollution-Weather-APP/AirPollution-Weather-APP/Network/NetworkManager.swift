aeaaa99dd3e9f801cf4f8efbdf09a5ec21a44e11
LeeSungNo-ian <isnn0103@gmail.com>
2022-10-10 02:11:15 +0900

    [#3] feat: NetworkManager 구현

--- AirPollution-Weather-APP/AirPollution-Weather-APP/Network/NetworkManager.swift
+++ AirPollution-Weather-APP/AirPollution-Weather-APP/Network/NetworkManager.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+
+final class NetworkManager {
+    
+    typealias NetworkCompletion = (Result<Co, NetworkError>) -> Void
+    
+    func fetchAirPollutionData(completion: @escaping NetworkCompletion) {
+        let urlString = RequestURL().airPollutionURL + APIKey().apiKey
+        performRequest(with: urlString) { result in completion(result) }
+    }
+    
+    private func performRequest(with urlString: String, completion: @escaping NetworkCompletion) {
+        guard let url = URL(string: urlString) else { return }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { data, response, error in
+            if error != nil {
+                print(error!)
+                completion(.failure(.networkingError))
+                
+                return
+            }
+            
+            guard let safeData = data else {
+                completion(.failure(.dataError))
+                
+                return
+            }
+            
+            if let youtubeData = self.parseJSON(safeData) {
+                completion(.success(youtubeData))
+            } else {
+                completion(.failure(.parseError))
+            }
+            
+        }
+        task.resume()
+    }
+    
+    private func parseJSON(_ weatherData: Data) -> Co? {
+        do {
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            let decodeData = try decoder.decode(AirPollutionDatasStruct.self, from: weatherData)
+            
+            return decodeData.data.iaqi.pm10
+        } catch {
+            print(error.localizedDescription)
+            
+            return nil
+        }
+    }
+}
+
