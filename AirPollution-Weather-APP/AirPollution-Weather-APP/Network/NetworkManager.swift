c03b994a021942671515026305abac7bef18391d
LeeSungNo-ian <isnn0103@gmail.com>
2022-10-16 21:02:26 +0900

    refactor: 사용하지 않는 함수 제거

--- AirPollution-Weather-APP/AirPollution-Weather-APP/Network/NetworkManager.swift
+++ AirPollution-Weather-APP/AirPollution-Weather-APP/Network/NetworkManager.swift
@@ -13,21 +13,10 @@ final class NetworkManager {
     typealias WeatherDataNetworkCompletion = (Result<Temp, NetworkError>) -> Void
     
     var airPollutionURL = ""
-    var weatherDataURL = ""
     
     func fetchAirPollutionData(completion: @escaping AirPollutionNetworkCompletion) {
         let urlString = self.airPollutionURL + APIKey().apiKey
         airPollutionPerformRequest(with: urlString) { result in completion(result) }
-    }
-    
-    func fetchWeatherData(completion: @escaping WeatherDataNetworkCompletion) {
-        let urlString = self.weatherDataURL + APIKey().apiKey
-        weatherDataPerformRequest(with: urlString) { result in completion(result) }
-    }
-    
-    func fetchSearchingWeatherData(cityName: String, completion: @escaping WeatherDataNetworkCompletion) {
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=\(APIKey().apiKey)"
-        weatherDataPerformRequest(with: urlString) { result in completion(result) }
     }
     
     private func airPollutionPerformRequest(with urlString: String, completion: @escaping AirPollutionNetworkCompletion) {
@@ -55,49 +44,12 @@ final class NetworkManager {
         task.resume()
     }
     
-    private func weatherDataPerformRequest(with urlString: String, completion: @escaping WeatherDataNetworkCompletion) {
-        guard let url = URL(string: urlString) else { return }
-        let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: url) { data, response, error in
-            if error != nil {
-                print(error!)
-                completion(.failure(.networkingError))
-                return
-            }
-            
-            guard let safeData = data else {
-                completion(.failure(.dataError))
-                return
-            }
-            
-            if let needData = self.parseWeatherDataJSON(safeData) {
-                completion(.success(needData))
-            } else {
-                completion(.failure(.parseError))
-            }
-            
-        }
-        task.resume()
-    }
-    
     private func parseAirPollutionJSON(_ weatherData: Data) -> [List]? {
         do {
             let decoder = JSONDecoder()
             decoder.dateDecodingStrategy = .iso8601
             let decodeData = try decoder.decode(AirPollutionDatasStruct.self, from: weatherData)
             return decodeData.list
-        } catch {
-            print(error.localizedDescription)
-            return nil
-        }
-    }
-    
-    private func parseWeatherDataJSON(_ weatherData: Data) -> Temp? {
-        do {
-            let decoder = JSONDecoder()
-            decoder.dateDecodingStrategy = .iso8601
-            let decodeData = try decoder.decode(WeatherInformation.self, from: weatherData)
-            return decodeData.temp
         } catch {
             print(error.localizedDescription)
             return nil
